fe0ea1c63b3a0492a9527b69ea6aa43443b0a962
Yuhao Zhao <yzhao2@bates.edu>
2024-04-18 23:03:52 -0400

    edit push-eric

--- code/Analysis/Summary_Stats.R
+++ code/Analysis/Summary_Stats.R
@@ -11,8 +11,9 @@ merged_unmeployed_claim_ny<-read_csv("data/Analysis_Data/merged_outcome_ny.csv")
 dep_ind_data<-merged_unmeployed_claim_ny %>% select(County, Year, Month, recip_rate, ec_county, clustering_county, volunteering_rate_county)%>% group_by(Year,Month,County)
 
 #summary stats table in latex format
-knitr::kable(describe(dep_ind_data)[, c(2:10)],  caption = "Summary Statistics of Dependent and Independent Variables", digits = 3, format = "latex")
+knitr::kable(describe(dep_ind_data)[, c(2:10)],  caption = "Summary Statistics of Dependent and Independent Variables", digits = 3, format = "latex", )
 
 #summary of variables by county
 #> merged_unmeployed_claim_ny %>% select(County, ec_county) %>% group_by(County) %>% skim() %>% select(County, numeric.mean, numeric.sd, numeric.p0, numeric.p25, numeric.p50, numeric.p75, numeric.p100)
+
 
